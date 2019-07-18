b3e38c79beb60a2b97b39bcb8922fa0e398ce043
manvinayaka <mann.vinayaka@gmail.com>
2019-07-18 13:52:39 +0530

    Update as of 18/7/19

--- Py/trendLevel.py
+++ Py/trendLevel.py
@@ -8,51 +8,25 @@ from numpy  import array
 import seaborn as sns
 
 df = pd.read_csv("mydata.csv")
-mydata = pd.read_csv("mydata.csv")
-
-pm10 = mydata.pm10
-o3 = mydata.o3
-ws = mydata.ws
-wd = mydata.wd
-nox = mydata.nox
-no2 = mydata.no2
-pm25 = mydata.pm25
-
-
-#df = df.drop("date", axis=1)
+df.index= pd.to_datetime(df.date)
 year = ['1998','1999','2000','2001','2002','2003']
-y =1
 
-while y<=6:
-    df = pd.read_csv("mydata.csv")
-    df.index= pd.to_datetime(df.date)
-    df_2003 = df[year[y-1]]
-    df_2003['month'] = df_2003.index.month
-    y = y+1
-    
-    t_0 = []
-    
-    #################
-    #fig,ax = plt.subplots(figsize=(10,10), nrows=2, ncols=3)
-    h = 0
-    while h <=23 :
-        
-        m = 1
-        while m <=12:
-            df = df_2003[df_2003.month==m]
-            a = df.groupby(df.index.hour).mean()
-        
-            test = []
-            test = a['nox']
-            t_0.append(test[h])
-            m = m+1
-        h = h+1
-    t_0 = np.reshape(t_0, (24, 12))
-    
-    ######################
-    
-    fig,ax = plt.subplots(figsize=(15,15))
-    ax = sns.heatmap(t_0, cbar = True,linewidth=0, cmap = 'Spectral_r',vmin=0, vmax=400)
-    plt.gca().invert_yaxis()
-    ax.set_title(year[y-2])
-    plt.show()
+def trend_level(df, pollutant, **kwargs):
+    """
+    df: pd.DataFrame with pd.DateTimeIndex as the index
+    """
+    pollutant_series = df[pollutant]
+    unique_years = np.unique(df.index.year)[:6]
+    num_unique_years = len(unique_years)
+    fig, ax = plt.subplots(nrows=num_unique_years, sharex=True, figsize=(10, 50))
+    for i, year in enumerate(unique_years):
+        year_string = f'{year}'
+        pollutant_series_year = pollutant_series[year_string]
+        t = pollutant_series_year.groupby([pollutant_series_year.index.month, pollutant_series_year.index.hour]).mean()
+        two_d_array= t.values.reshape(12, 24).T
+        sns.heatmap(two_d_array, cbar = True,linewidth=0, cmap = 'Spectral_r',vmin=0, vmax=400, ax=ax[i])
+        ax[i].set_title(year_string)
+        ax[i].invert_yaxis()
+    return ax
+
+trend_level(df,'nox')
