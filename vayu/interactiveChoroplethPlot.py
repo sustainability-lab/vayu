a85663925305c16fed673a47090a941edfef71d1
Tanmay Srivastava <sayitalltometanmay@gmail.com>
2020-05-09 22:53:53 +0530

    Delete interactiveChoroplethPlot.py

--- vayu/interactiveChoroplethPlot.py
+++
@@ -1,3 +0,0 @@
-map_ = interactiveChoroplethPlot(gdf, df, pollutant = 'pm25', country = 'India', date_time_col_name = "time", value_col_name = "val",
-                              dist_col_name = "0", start_date = "2019-08-20-06:00", end_date = "2019-08-24-06:00",
-                              opacity = 0.4)
